24725501c160795ad80a53a0408f5e2f999247c4
fixbug <1911626201@qq.com>
2024-09-23 22:55:51 +0800

    CPP

---
+++ CPP/day05.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+class Complex{
+    public:
+        Complex(int real = 1,int image = 2):real(real),image(image){}
+        int real;
+        int image;
+        Complex operator+(Complex& C1) {
+            Complex C2;
+            C2.real = this->real + C1.real;
+            C2.image = this->image + C1.image; // 使用 C1.image
+            return C2;
+        }
+
+        Complex& operator=(Complex& C1){
+            this->image = C1.image;
+            this->real = C1.real;
+            return *this;
+        }
+
+        bool operator==(Complex& C1){
+            return ((C1.image == this->image) && (C1.real == this->real));
+        }
+
+        Complex& operator+=(Complex& C1){
+            this->image += C1.image;
+            this->real += C1.real;
+            return *this;
+        }
+
+        Complex& operator-=(Complex& C1){
+            this->image -= C1.image;
+            this->real -= C1.real;
+            return *this;
+        }
+
+        Complex& operator*=(Complex& C1){
+            this->image *= C1.image;
+            this->real *= C1.real;
+            return *this;
+        }
+};
+
+std::ostream &operator<<(std::ostream &cout, const Complex& C1) {
+    cout << C1.real << " " << C1.image;
+    return cout;
+}
+
+std::istream &operator>>(std::istream &cin, Complex& C1) {
+    cin>>C1.real>>C1.image;
+    return cin;
+}
+
+class Mail{
+    public:
+        Mail(const char* Ctx,const char* Til):Context(Ctx),Title(Til)
+        {}
+        std::string Context;
+        std::string Title;
+};
+
+class Sender{
+    public:
+        void operator<<(Mail &mail){
+            std::cout<<mail.Title<<"  "<<mail.Context<<std::endl;
+            return;
+        }
+};
+
+
+int main(){
+    Complex C1(1,2);
+    std::cout<<C1<<std::endl;
+    std::cin>>C1;
+    std::cout<<C1<<std::endl;
+
+    Sender sender;
+    Mail mail("Hello World","HaHaHa");
+    sender<<mail;
+}
